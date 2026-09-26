0f715cd71b13a21d64a16e00c78e801f5da77ac7
Aryan Tawde <aryantawade123@gmail.com>
2026-09-26 12:55:38 +0530

    Time: 1 ms (99.94%) | Memory: 94.4 MB (51.59%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,11 @@
+
 class Solution {
     public int maxProfit(int[] prices) {
-       int minPrice=Integer.MAX_VALUE;
-       int maxProfit=0;
-       for(int i=0;i<prices.length;i++){
-        if(prices[i]<minPrice){
-            minPrice=prices[i];}
-            else if(prices[i]-minPrice>maxProfit){
-                maxProfit=prices[i]-minPrice;
-            }
+        int minPrice = Integer.MAX_VALUE, maxProfit = 0;
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price); // Sasta stock update karein
+            maxProfit = Math.max(maxProfit, price - minPrice); // Best profit update karein
         }
         return maxProfit;
-       }
+    }
 }
